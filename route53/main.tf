5e8f8c52a3854e318aa195d747d7a827bdbfc3a6
Phillip Hocking <phillhocking@gmail.com>
2021-07-09 03:22:31 -0700

    add route53 zone for cf distribution facilitated by amplify

---
+++ route53/main.tf
@@ -0,0 +1,28 @@
+## Creates a new Route53 Public Hosted Zone. Comment out for existing zone.
+resource "aws_route53_zone" "app-domain" {
+  name = "${var.domain_name}."
+}
+
+## Points to your existing Route 53 public hosted zone. Comment this block if you are creating a new Public Hosted Zone
+#data "aws_route53_zone" "hosted_zone" {
+#  name = "${var.domain_name}." 
+#}
+
+resource "aws_route53_record" "app-domain" {
+  zone_id = aws_route53_zone.app-domain.zone_id    ## Delete prepending "data." if you are creating a new hosted zone
+  name    = "${aws_route53_zone.app-domain.name}." ## Delete prepending "data." if you are creating a new hosted zone
+  type    = "A"
+
+  alias {
+    name                   = element([aws_amplify_domain_association.this.sub_domain.dns_record], 1)
+    zone_id                = aws_route53_zone.app-domain.zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "green_www" {
+  zone_id = aws_route53_zone.green_no_www.zone_id        ## Delete prepending "data." if you are creating a new hosted zone
+  name    = "www.${aws_route53_zone.green_no_www.name}." ## Delete prepending "data." if you are creating a new hosted zone
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${var.domain_name}."]
