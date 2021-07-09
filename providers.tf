e32a22e108724e5d0bf1ebe7f1da0a688210a627
Phillip Hocking <phillhocking@gmail.com>
2021-07-08 23:14:51 -0700

    put config into provider block

--- providers.tf
+++ providers.tf
@@ -5,14 +5,17 @@ required_providers {
     aws = {
       source     = "hashicorp/aws"
       version    = "~> 3.32"
-      region     = var.aws_region
-      access_key = var.access_key
-      secret_key = var.secret_key
     }
 
     local = {
       source  = "hashicorp/local"
       version = "~> 2.1"
     }
+  }
+
+  provider aws {
+    region      = var.aws_region
+     access_key = var.access_key
+     secret_key = var.secret_key
   }
 }
